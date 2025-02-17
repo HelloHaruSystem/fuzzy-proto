7cbf2e0a949c9a5e992387189b1aac30446fc138
Halfdan Sand <halfdansc@gmail.com>
2025-02-17 12:17:59 +0100

    moved inventory methods to service class

--- oopProto/Entities/Services/PlayerService.cs
+++ oopProto/Entities/Services/PlayerService.cs
@@ -1,4 +1,6 @@
-﻿namespace oopProto.Entities.Services;
+﻿using oopProto.ItemsAndInventory;
+
+namespace oopProto.Entities.Services;
 
 public class PlayerService
 {
@@ -32,10 +34,58 @@ public class PlayerService
         }
     }
     
-    //TODO: move to service class
+    // TODO: move Console.Write() and Console.WriteLine() methods to the ui
     public void ShowInventory()
     {
-        this.player.PlayerInventory.ShowInventory();
+        Console.WriteLine("Items:");
+        for (int i = 0; i < this.player.PlayerInventory.Items.Count; i++)
+        {
+            if (i % 5 == 0)
+            {
+                Console.WriteLine();
+            }
+
+            Console.Write($"[1]: {this.player.PlayerInventory.Items[i]}\t");
+        }
+    }
+    
+    // TODO: move the Console.WriteLine() method to the ui
+    public void AddItem(Item item)
+    {
+        if (this.player.PlayerInventory.CurrentCapacity >= this.player.PlayerInventory.MaxCapacity)
+        {
+            Console.WriteLine("Inventory is full");
+        }
+        else
+        {
+            this.player.PlayerInventory.Items.Add(item);
+            this.player.PlayerInventory.CurrentCapacity++;
+        }
+    }
+    
+    // TODO: move the Console.WriteLine() method to the ui
+    public void SwapItems(Item itemToReplace, Item newItem)
+    {
+        if (this.player.PlayerInventory.Items.Contains(itemToReplace))
+        {
+            this.player.PlayerInventory.Items.Remove(itemToReplace);
+            this.player.PlayerInventory.Items.Add(newItem);
+        }
+        else
+        {
+            Console.WriteLine("Item not found");
+        }
+    }
+    
+    public void RemoveItem(Item item)
+    {
+        this.player.PlayerInventory.Items.Remove(item);
+        this.player.PlayerInventory.CurrentCapacity--;
+    }
+    
+    public void AddMaxCapacity(int increasedCapacity)
+    {
+        this.player.PlayerInventory.MaxCapacity += increasedCapacity;
     }
     
     // getters and setters
