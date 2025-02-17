f4e89142aaeb3d0b74b6d0821c34e7e942ecaf29
Halfdan Sand <halfdansc@gmail.com>
2025-02-17 08:25:16 +0100

    worked on the user interface

--- oopProto/UserInterface/UserInput/Commands.cs
+++ oopProto/UserInterface/UserInput/Commands.cs
@@ -2,8 +2,30 @@
 
 public class Commands
 {
-    public static void selectCommand(String)
+    public static void selectCommand(Player player)
     {
-        switch ()
+        string userInput = Console.ReadLine().ToLower();
+
+        switch (userInput)
+        {
+            case "go north":
+                player.moveRoom("north");
+                break;
+            case "go east":
+                player.moveRoom("east");
+                break;
+            case "go south":
+                player.moveRoom("south");
+                break;
+            case "go west":
+                player.moveRoom("west");
+                break;
+            case "show inventory":
+                player.showInventory();
+                break;
+            default:
+                Console.WriteLine("Invalid command");
+                break;
+        }
     }
 }
