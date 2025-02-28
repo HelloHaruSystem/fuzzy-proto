e8c337ad8abca2c645e108c36c2c819ccac39cc6
Halfdan Sand <halfdansc@gmail.com>
2025-02-28 14:02:30 +0100

    added the class LoadOrNew that will be used when starting the game

--- oopProto/UserInterface/UserInput/LoadOrNew.cs
+++ oopProto/UserInterface/UserInput/LoadOrNew.cs
@@ -1,9 +1,39 @@
-﻿namespace oopProto.UserInterface;
+﻿using oopProto.Entities.Factory;
+
+namespace oopProto.UserInterface;
 
 public class LoadOrNew
 {
-    public static void StartNewOrLoad()
+    public static async Task<GameUi> StartNewOrLoad()
     {
-        Console.WriteLine("Welcome to the Oop game!\nDo you want to load an existing game?\n");
+        GameUi? ui = null;
+        string userInput = "";
+        bool validInput = false;
+        
+        Console.WriteLine("Welcome to the Oop game!\nDo you want to load an existing game?\nOr start a new game?");
+
+        while (!validInput)
+        {
+            Console.WriteLine("Enter [1] to start a new game");
+            Console.WriteLine("Enter [2] to load an existing game\n> ");
+            userInput = Console.ReadLine();
+            
+            switch (userInput)
+            {
+                case "1":
+                    ui = await GameUiFactory.CreateGameUiFromScratch();
+                    validInput = true;
+                    break;
+                case "2":
+                    ui = await GameUiFactory.CreateGameUiFromSave();
+                    validInput = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid input. Try again.\n> ");
+                    break;
+            }
+        }
+
+        return ui;
     }
 }
